4d1454d3a339efa6984ee3f1cf03a2d52eac3774
samuellungu <samuellungu97@gmail.com>
2022-09-25 06:59:23 +0200

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("\nHello World");
+        System.out.println(System.getProperty("java.home"));
+        //System.out.println(System.getProperty("java.library.path"));
+        //System.out.println(System.getProperty("java.ext.dirs"));
+        System.out.println(System.getProperty("java.class.path"));
+
+
+    }
+}
---
+++ Stack.java
@@ -0,0 +1,36 @@
+public class Stack<Item> {
+    private Node first;
+    private int N;
+
+    private class Node {
+        Node next;
+        Item item;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void push(Item item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+        N++;
+    }
+
+    public Item pop() {
+        Item item = first.item;
+        first = first.next;
+        N--;
+        return item;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
